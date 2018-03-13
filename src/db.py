fd88b9e671c15d6d2bdb42b1b9a4c35493b2ec28
SarvarD <sdhillon4939@gmail.com>
2018-03-12 22:51:17 -0400

    begin heavier descriptions of underlying schema tables, begin to identify and address some ambiguities

--- src/db.py
+++ src/db.py
@@ -1,6 +1,7 @@
 import sqlalchemy
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 
 # Used to keep track of classes and tables
 from sqlalchemy.ext.declarative import declarative_base
@@ -36,10 +37,63 @@ class Game(Base):
     home = Column(String)
     # Can use date and home as a primary key: A team can only play one game at any given time
     visitor = Column(String)
-    home_score = Column(Integer)
-    visitor_score = Column(Integer)
+    winner = Column(String)
+    loser = Column(String)
+    # home_score = Column(Integer)
+    # visitor_score = Column(Integer)
     isLeague = Column(Boolean)
     isPlayoff = Column(Boolean)
+    # How do we store period data - i.e. what if there's 12 OT? Should use PostgreSQL with json
+
+
+class Team(Base):
+    team_id = Column(String, primary_key=True)
+    name = Column(String)
+
+
+class PlaysIn(Base):
+    team = Column(String, ForeignKey('teams.team_id'))
+    game = Column(Integer, ForeignKey('game.id'))
+
+    # Box Stats
+    fgm = Column(Integer)   # Made field goals
+    fga = Column(Integer)   # Attempted field goals
+
+    fgm3 = Column(Integer)  # Made threes
+    fga3 = Column(Integer)  # Attempted threes
+
+    ftm = Column(Integer)   # Made free throws
+    fta = Column(Integer)   # Attempted free throws
+
+    tp = Column(Integer)    # Total points
+    blk = Column(Integer)   # Total blocks
+    stl = Column(Integer)   # Total steals
+    ast = Column(Integer)   # Total assists
+    oreb = Column(Integer)  # Total offensive rebounds
+    dreb = Column(Integer)  # Total defensive rebounds
+    treb = Column(Integer)  # Total rebounds (offensive + defensive)
+    pf = Column(Integer)    # Total personal fouls
+    tf = Column(Integer)    # Total team fouls
+    to = Column(Integer)    # Total turnovers
+
+    # Special Statistics
+    is_home = Column(Boolean)
+    pts_to = Column(Integer)        # Points scored off of turnovers
+    pts_paint = Column(Integer)     # Points scored in the paint
+    pts_ch2 = Column(Integer)       # Points scored within the arc but outside the paint?
+    pts_fastb = Column(Integer)     # Points scored on fastbreaks
+    pts_bench = Column(Integer)     # Points scored by the bench
+    ties = Column(Integer)          # Number of times the team tied the game
+    leads = Column(Integer)         # Number of times the team took the lead
+    poss_count = Column(Integer)    # Number of possessions
+    poss_time = Column(Integer)     # Amount of time the team had the ball
+    score_count = Column(Integer)   # Amount of possessions the team scored on
+    score_time = Column(Integer)    # Amount of time spent on possessions that resulted in scoring TODO: verify this
+
+
+
+
+
 
 
 
