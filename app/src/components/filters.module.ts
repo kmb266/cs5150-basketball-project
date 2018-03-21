e836ec5f3c1bdd11dd1cd1654774063a1adca066
Kyle Brown <Kyle@Kyles-MacBook-Air.local>
2018-03-20 22:27:17 -0400

    figured out how to make external js libraries work with angular2 and electron and added range slider in player filter (which we can copy to other filters)

--- app/src/components/filters.module.ts
+++ app/src/components/filters.module.ts
@@ -1,11 +1,12 @@
 import { NgModule} from '@angular/core';
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 import { PlayersFilterComponent } from "./filter.players.component"
 import { TeamsFilterComponent } from "./filter.teams.component"
 import { GamesFilterComponent } from "./filter.games.component"
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [PlayersFilterComponent, TeamsFilterComponent, GamesFilterComponent],
   declarations: [PlayersFilterComponent, TeamsFilterComponent, GamesFilterComponent],
   providers: []
--- app/src/global.vars.ts
+++ app/src/global.vars.ts
@@ -2,3 +2,24 @@
 export const navHeight: number = 50;
 export const pages: Array<string> = ["players", "teams","games"];
 export const numPages: number = pages.length;
+export const secondsToGametime = (totalSeconds) => {
+  if (Math.abs(totalSeconds) > 1200) {
+    var s = Math.abs(totalSeconds) - 20*60;
+    var minutes = Math.floor(s / 60);
+    var seconds = s % 60;
+    if (minutes < 10) {minutes = '0'+ minutes;}
+    if (seconds < 10) {seconds = '0'+ seconds;}
+    return minutes+':'+seconds
+  }
+  var s = Math.abs(totalSeconds);
+  var minutes = Math.floor(s / 60);
+  var seconds = s % 60;
+  if (minutes < 10) {minutes = '0'+ minutes;}
+  if (seconds < 10) {seconds = '0'+ seconds;}
+  return minutes+':'+seconds;
+}
+export const gametimeToSeconds = (gametime, isSecondHalf) => {
+  var minSec = gametime.split(":");
+  if (isSecondHalf) return parseInt(minSec[0])*60 + parseInt(minSec[1]);
+  return parseInt(minSec[0])*60 + parseInt(minSec[1]) + 1200;
+}
