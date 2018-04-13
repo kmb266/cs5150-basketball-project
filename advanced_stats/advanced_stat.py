9708b9448a0832aaebe1e625f46931cd41ffe7fb
Natalie Rain <31663983+yingjd@users.noreply.github.com>
2018-04-13 16:25:10 -0400

    update

--- advanced_stats/advanced_stat.py
+++ advanced_stats/advanced_stat.py
@@ -5,11 +5,24 @@ import sys;
 
 def TmPoints():
 	pass
+	"""
+	Name:total points by team 
+
+	"""
+
 def OREB_perc():
 	pass
+	"""
+	Name:Total Offensive Rebounds Percentage 
+
+	"""
 
 def DOREB_perc():
 	pass 
+	"""
+	Name: Total Defensive Rebounds Percentage (double check)
+
+	"""
 
 
 
@@ -20,31 +33,27 @@ def TmPoss(FGA, TmOREB, oppDREB,FTA,FGM):
 	Returns: the total team possession based on parameters
 	Arguments:
 		FGA: field goals attemped 
-		TmOREB: ??
-		oppDREB:  ?? 
+		TmOREB: Team Total Offensive Rebounds (double check) 
+		oppDREB:  Opponent Total Defensive Rebounds (double check) 
 		FTA: free throw attemped 
 		FGM: field goals made 
 		
 	
-	Depend? Yes 
+	Depend? No 
 	check status: No 
 
 	"""
 
-def Team_Offensive_Rating (TmPoints, TmPoss):
+def ORTG (TmPoints, TmPoss):
 	return 100*(TmPoints/TmPoss)
 	"""
-	Name: Total Team Possesion 
-	Returns: the total team possession based on parameters
+	Name: Team Offensive Rating
+	Returns: the team offensice rating based on parameters
 	Arguments:
-		FGA: field goals attemped 
-		TmOREB: ??
-		oppDREB:  ?? 
-		FTA: free throw attemped 
-		FGM: field goals made 
-		
-	
-	Depend? Yes 
+		TmPoints: total points by team 
+		TmPoss: total team possestion 
+
+	Depend? No 
 	check status: No 
 
 	"""
@@ -56,29 +65,103 @@ def PProd(PProdFG, PProdAst,FTM,TmOREB,
 		TMScorPoss,TmOREBWgt,TmPlay,PProdOREB):
 	return (PProdFG+PProdAst+FTM)*(1-(TmOREB/TMScorPoss)
 			*TmOREBWgt*TmPlay_pect)+PProdOREB
+	"""
+	Name: Individual Points Produced 
+	Returns: the individual points produced based on parameters
+	Arguments:
+		PProdFG: points produced FG part 
+		PProdAst: points produced AST part 
+		FTM: free throw made 
+		TmOREB: Team Total Offensive Rebounds(double check) 
+		TMScorPoss:  ????
+		TmOREBWgt: team OREB weight  
+		TmPlay: team play 
+		PProdOREB: points produced OREB part 
+		
+	Depend? Yes 
+	check status: No 
+
+	"""
+
 
 
 def PProdFG(FGM,PTS,FTM,FGA,qAST):
 	return 2*(FGM+0.5*FGM_3)*1-0.5*((PTS-FTM)/(2*FGA))*qAST
+	"""
+	Name: Points Produced Field Goal Part 
+	Returns: the Points Produced Field Goal Part based on parameters
+	Arguments:
+		FGM: field goals made 
+		PTS: points (double check)
+		FTM: free throws made 
+		FGA: field goals attemped 
+		qAST: ???? 
+
+	Depend? Yes 
+	check status: No 
+
+	"""
 
 
 def PProdAst(TmFGM, FGM, Tm3PM, TmPTS, TmFTM, PTS, FGA, AST):
-	return 2*((TmFGM-FGM+0.5*(Tm3PM-FGM_3))/(TmFGM-FGM))*0.5 \
+	return 2*((TmFGM-FGM+0.5*(Tm3PM-FGM_3))/(TmFGM-FGM))*0.5
 			*(((TmPTS-TmFTM)-(PTS-FTM))/(2*(TmFGA-FGA) ))*AST
+	"""
+	Name: Points Produced Assist Part 
+	Returns: the Points Produced Assist Part based on parameters
+	Arguments:
+		TmFGM: team field goal made 
+		FGM: field goal made 
+		Tm3PM: team 3 points 
+		TmPTS: team points 
+		TmFTM: team free throw made 
+		PTS: points 
+		FGA: field goal made 
+		AST: assist 
+		
+	Depend? No  
+	check status: No 
+
+	"""
 
 
 def PProdOREB(ORB, TmOREBWgt,TmPlay, TmPTS,
 			TmFGM,TmFTM,TmFTA):
 	return ORB*TmOREBWgt*TmPlay_pect*(TmPTS/(TmFGM+(1-(1-(TmFTM/TmFTA))^2 )
 			*0.4*TmFTA))
+	"""
+	Name: Points Produced Total Offensive Rebounds Part 
+	Returns: the Points Produced Total Offensive Rebounds Part based on parameters
+	Arguments:
+		ORB: ????
+		TmOREBWgt: team OREB weight
+		TmPlay: team play 
+		TmPTS: team points 
+		TmFGM: team field goal made 
+		TmFTM: team free throw made 
+		TmFTA: team free theow attemped 
+		
+	Depend? Yes   
+	check status: No 
 
-
-
-
+	"""
 
 
 def TotPoss(ScPoss,FGmPoss,FTmPoss,TOV):
 	return ScPoss+FGmPoss+FTmPoss+TOV
+	"""
+	Name: Individual Total Possession 
+	Returns: the Individual Total Possession based on parameters
+	Arguments:
+		ScPoss: Scoring Possessions 
+		FGmPoss: Field Goal Missed Possessions 
+		FTmPoss: Free Throw Missed Possessions 
+		TOV: trunovers (available since the 1977-78 season in the NBA)
+		
+	Depend? Yes   
+	check status: No 
+
+	"""
 
 
 
@@ -86,46 +169,196 @@ def ScPoss(FGPart,ASTPart,FTPart,TmOREB,
 			TmScorPoss,TmOREBWgt,TmPlay,OREBPart):
 	return (FGPart+ASTPart+FTPart)*(1-(TmOREB/TmScorPoss)
 		*TmOREBWgt*TmPlay_pect)+OREBPart
+	"""
+	Name: Scoring Possessions 
+	Returns: the Scoring Possessions based on parameters
+	Arguments:
+		FGPart: field goal part (double check)
+		ASTPart: assist part (double check)
+		FTPart: free throw part (double check )
+		TmOREB: team Total Offensive Rebounds
+		TmScorPoss: team scoring sossessions  
+		TmOREBWgt: team Total Offensive Rebounds weight
+		TmPlay: team play 
+		OREBPart: Total Offensive Rebounds part 
+
+	Depend? Yes   
+	check status: No 
+
+	"""
 
 
 def FGPart(FGM,PTS,FTM,FGA,qAST):
 	return FGM*(1-0.5*(((PTS-FTM))/(2*FGA))*qAST)
+	"""
+	Name: Field Goal Part 
+	Returns: the Field Goal Part based on parameters
+	Arguments:
+		FGM: Fieldgoals made
+		PTS: points 
+		FTM: free throw made 
+		FGA: field goal made 
+		qAST: ????
+		
+
+		Depend? Yes 
+		check status: No 
+
+	"""
 
 
 def qAST(MIN,TmMin,TmMIN):
 	return (MIN/(TmMin/5))*q_5+(1-(MIN/(TmMIN/5))*q_12)
+	"""
+	Name: ??????
+	Returns: the ???? based on parameters
+	Arguments:
+		MIN:  ?? 
+		TmMin: ??? 
+		TmMIN: ????
+	
+		Depend? Yes 
+		check status: No 
+
+	"""
 
 
 def q5(TmAST,AST,TmFGM):
 	return 1.14*((TmAST-AST)/TmFGM)
+	"""
+	Name: ???
+	Returns: the ????? based on parameters
+	Arguments:
+		TmAST: team assist 
+		AST: assist 
+		TmFGM: team field goal made 
+
+		Depend? No  
+		check status: No 
+
+	"""
 
 
 def q12(TmAST,TmMIN,MIN,AST,TmFGM,FGM):
 	return ((TmAST/TmMIN)*MIN*5-AST)/((TmFGM/TmMIN)*MIN*5-FGM)
+	"""
+	Name: ???
+	Returns: the ????? based on parameters
+	Arguments:
+		TmAST: team assist 
+		TmMIN: ???
+		MIN: ????
+		AST: assist 
+		TmFGM: team field goal made 
+		FGM: field goal made 
+
+		Depend? Yes   
+		check status: No 
+
+	"""
 
 
 def ASTPart(TmPTS,TmFTM,PTS,FTM,TmFGA,FGA,AST):
 	return 0.5*(((TmPTS-TmFTM)-(PTS-FTM))/(2*(TmFGA-FGA) ))*AST
+	"""
+	Name: Assist Part 
+	Returns: the assist part based on parameters
+	Arguments:
+		TmPTS: team points 
+		TmFTM: team free throw made 
+		PTS: points 
+		FTM: free throw made 
+		TmFGA: team field goal attemped 
+		FGA: field goal attemped 
+		AST: assist 
+		
+
+		Depend? No  
+		check status: No 
+
+	"""
 
 
 def FTPart(FTM,FTA):
 	return (1-(1-(FTM/FTA))^2 )*0.4*FTA
+	"""
+	Name: Free Throw Part 
+	Returns: the free throw part based on parameters
+	Arguments:
+		FTM: free throw made 
+		FTA: free throw attemped 
+
+		Depend? No  
+		check status: No 
+
+	"""
 
 
-def TmOREBWgt(TmFGM,TmFTM,TmFTA):
+
+def TmScorPoss(TmFGM,TmFTM,TmFTA):
 	return TmFGM+(1-(1-(TmFTM/TmFTA))^2 )*TmFTA*0.4
+	"""
+	Name: Team Scoring Possession 
+	Returns: the Team Scoring Possession based on parameters
+	Arguments:
+		TmFGM: team field goal made 
+		TmFTM: team free throw made 
+		TmFTA: team three throw attemped 
+
+		Depend? No  
+		check status: No 
+
+	"""
 
 
 def TmOREBWgt(TmOREB,TmPlay):
 	return ((1-TmOREB_pect)*TmPlay_pect)/((1-TmOREB_pect)
 			*TmPlay_pect+TmOREB_pect*(1-TmPlay_pect))
+	"""
+	Name: Team Total Offensive Rebounds Weight 
+	Returns: the Team Total Offensive Rebounds Weight based on parameters
+	Arguments:
+		TmOREB: team Total Offensive Rebounds
+		TmPlay: team play 
+
+		Depend? No  
+		check status: No 
+
+	"""
+
 
 def TmOREB_pect(TmOREB,OppTREB):
 	return TmOREB/(TmOREB+(OppTREB-OppDREB))
+	"""
+	Name: Team Total Offensive Rebounds Weight Percentage  
+	Returns: the Team Total Offensive Rebounds Weight Percentage based on parameters
+	Arguments:
+		TmOREB: team Total Offensive Rebounds
+		OppTREB:  ????????? 
+
+		Depend? Yes   
+		check status: No 
+
+	"""
 
 
 def TmPlay(TmFGA,TmFTA,TmTOV):
 	return TmFGA+TmFTA*0.4+TmTOV
+	"""
+	Name: Team Play
+	Returns: the Team Play based on parameters
+	Arguments:
+		TmFGA: team field goal attemped 
+		TmFTA: team free throw attemped 
+		TmTOV: team turnover 
+
+		Depend? No    
+		check status: No 
+
+	"""
+
+
+
 
 
 def TmPlay_pect(TmScorPoss, TmFGA,TmFTA,TmTOV):
@@ -133,16 +366,25 @@ def TmPlay_pect(TmScorPoss, TmFGA,TmFTA,TmTOV):
 
 
 
+
+
 def OREBPart(OREB,TmOREBWgt,TmPlay_pect):
 	return OREB*TmOREBWgt*TmPlay_pect
+
+
+
 
 
 def FGmPoss(FGA,FGM,TmOREB_pect):
 	return (FGA-FGM)*(1-1.07*TmOREB_pect)
 
 
+
+
 def FTmPoss(FTM,FTA):
 	return ((1-(FTM/FTA))^2 )*0.4*FTA
+
+
 
 
 def Individual_Offensize_Rating(PProd, TotPoss):
@@ -164,13 +406,19 @@ def TmDRTG(OppPTS,TmPoss):
 	return 100*(OppPTS/TmPoss)
 
 
+
+
 def DRTG(TmDRTG,OppPtsPScorPoss,top_perc,mDRTG):
 	return TmDRTG+0.2*(100*OppPtsPScorPoss*(1-Stop_perc)-TmDRTG)
 
 
 
+
+
 def OppPtsPScorPoss(OppPTS,OppFGM,OppFTM,OppFTA):
 	return OppPTS/(OppFGM+(1-(1-(OppFTM/OppFTA))^2 )*OppFTA*0.4)
+
+
 
 
 
@@ -200,6 +448,7 @@ def FMwt(DFG_perc,DOREB_perc):
 
 def DOREB_perc(OppOREB,TmDREB):
 	return OppOREB/(OppOREB/TmDREB)
+
 
 
 def DFG_perc(OppFGM,OppFGA):
@@ -431,7 +680,7 @@ def Pace(TmPoss,OppPoss,TmMIN):
 	Arguments:
 		TmPoss: Total Team Possesion   
 		OppPoss: Opponent Possesion (check!)
-		TmMIN: 
+		TmMIN: ????? 
 		
 	
 	Depend? Yes 
