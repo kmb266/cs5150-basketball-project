a37cc33e077d1e8077a7fcadd83258f22737a593
Yiqi Yu <yy757@cornell.edu>
2018-04-27 16:11:02 -0400

    add dropdown menu

--- app/auto_complete.py
+++ app/auto_complete.py
@@ -1,34 +1,66 @@
 import Constants
 import sys, json, numpy as np
+import ast
 
 '''
 get the form information from the front end
 '''
 
 def getErrorForm(code, msg):
-  data = {"error" : {
-            "code" : code,
-            "message" : msg
-            }
-          }
-  return data
+    data = {"error" : 
+                {
+                "code" : code,
+                "message" : msg
+                }
+           }
+    return data
+
+def getQueryForm(target, form):
+	queryForm = {"field" : target}#, "user_input" : form["user_input"]}
+
+	if target == Constants.AC_PLAYER:
+		team_id = form["team_id"]
+		if form is None:
+			return None
+		queryForm["team_id"] = team_id
+
+	return queryForm
 
 def retrieveData(form):
-  target = form["target"]
-  if target is None:
-    return getErrorForm(Constants.FormMissingElement, "Missing value for the key 'target'")
-  else:
-    if target == Constants.AC_POSITION:
-      return {"field" : Constants.AC_POSITION,
-              "data" : ["PG", "SG", "SF", "PF", "C"]}
-    else if target == Constants.AC_TEAM:
-      return {"field" : Constants.AC_TEAM,
-              "data" : ["Cornell University", "Dartmouth College", "Princeton University", "Harvard University", "Yale University"]}
-    else if target == Constants.AC_PLAYER:
-      #TO-DO: Add sample players
-      return {"field" : Constants.AC_PLAYER}
+    target = form["field"]
+    if target is None:
+        return getErrorForm(Constants.FormMissingElement, "Missing value for the key 'field'")
+
     else:
-      return getErrorForm(Constanst.InvalidFormValue, "Wrong value for key 'target'")
+        # position drop down menu 
+        if target == Constants.AC_POSITION:
+            return {"field" : Constants.AC_POSITION,
+                    "data" : ["PG", "SG", "SF", "PF", "C"]}
+        else:
+    	    queryForm = getQueryForm(target, form)
+    	    if queryForm is None:
+                return getErrorForm(Constants.FormMissingElement, "Missing value for the key 'team_id'")
+
+    	    #TO-DO: Delete this segment after backend is connected --------------------
+            # team drop down menu 
+    	    if target == Constants.AC_TEAM:
+      		    return {"field" : Constants.AC_TEAM,
+                        "data" : [{"id" : 1502, "name" : "Cornell University"}, 
+              			          {"id" : 1603, "name" : "Dartmouth College"},
+              			          {"id" : 1902, "name" : "Princeton University"},
+              			          {"id" : 1807, "name" : "Harvard University"},
+              			          {"id" : 1697, "name" : "Yale University"}]}
+
+            # player drop down menu 
+    	    elif target == Constants.AC_PLAYER:
+                return {"field" : Constants.AC_TEAM,
+                        "data" : [{"id" : 1502, "jersey" : 1,  "name" : "Kyle Brown"}, 
+                                  {"id" : 1603, "jersey" : 10, "name" : "Matt Morgan"},
+                                  {"id" : 1902, "jersey" : 12, "name" : "Jordan Abdur Ra'oof"},
+                                  {"id" : 1807, "jersey" : 32, "name" : "Jack Gordon"}]}
+            else:
+      		    return getErrorForm(Constants.InvalidFormValue, "Wrong value for key 'field'")
+      	#----------------------------------------------
 
 def read_in():
     lines = sys.stdin.readlines()
@@ -36,17 +68,22 @@ def read_in():
     return json.loads(lines[0])
 
 def getForm():
-    #lines = read_in()
-    #np_lines = np.array(lines)
-    np_lines = []
-    return np_lines
+    # lines = read_in()
+    # np_lines = np.array(lines)
+    # np_lines = []
+    # return np_lines
+
+    # TODO : delete reading lines from command, read from front end
+    np_lines = raw_input()
+    return json.loads(np_lines)
 
 
 def main():
     form = getForm()
-
-    #TO-DO: delete bottom line when getting form
-    form = {"field" : Constants.AC_POSITION}
+    form = ast.literal_eval(form)
+    
+    # TO-DO: delete bottom line when getting form
+    # form = {"field" : Constants.AC_TEAM}
 
     data = retrieveData(form)
 
@@ -57,4 +94,4 @@ def main():
 
 #start process
 if __name__ == '__main__':
-  main()
+    main()
