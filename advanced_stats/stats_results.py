186762ed5f0924f757d2d24ac129f9304cf1642b
Natalie Rain <31663983+yingjd@users.noreply.github.com>
2018-05-06 00:10:34 -0400

    PYTHON

---
+++ advanced_stats/stats_results.py
@@ -0,0 +1,56 @@
+import advanced_stat; 
+import numpy;
+
+
+def stats_calculation(data):
+	for dic in data:
+
+
+		#get basic data 
+
+		AST = 1
+		BLK
+		DREB
+		FGM = 2 
+		FGA = 1 
+		FTA 
+		FGM_2
+		FGA_2 
+		FGM_3 = 1 
+		FGA_3 
+		FTM  = 1 
+		MIN
+		ORE
+		OREB_perc = 2 
+		oppDREB
+		oppTREB 
+		oppOREB 
+		oppFGM 
+		oppFGA 
+		oppFTA 
+		oppFTM
+		oppTOV 
+		opp3PA
+		PF 
+		PTS = 1 
+		STL 
+		TmAST = 2 
+		TmPoints = 3 
+		TmFGA = 4 
+		TmOREB
+		TmDREB 
+		TmFTA
+		TmSTL 
+		TmFTM
+		TmFGM
+		TmFGA 
+		TmTOV
+		Tm3PM 
+		TmMIN 
+		TmPF
+		TmTREB 
+		TOV 
+		TREB   
+		
+
+		 
