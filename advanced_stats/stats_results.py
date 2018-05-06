16c24f18c587248616f859e7dd9c5c53d66c3fdc
Natalie Rain <31663983+yingjd@users.noreply.github.com>
2018-05-06 16:22:56 -0400

    update
    hahahahahhahaha

--- advanced_stats/stats_results.py
+++ advanced_stats/stats_results.py
@@ -27,7 +27,7 @@ def stats_calculation(data):
 		OppFGM = 1 
 		OppFGA = 1 
 		OppFTA = 1 
-		OppPTS
+		OppPTS = 1 
 		OppFTM = 1 
 		OppTOV = 1 
 		Opp3PA = 1 
@@ -85,6 +85,40 @@ def stats_calculation(data):
 		Game_Score = Game_Score(PTS,FGM,FGA,FTA,FTM,ORE,DREB,STL,AST,BLK,PF,TOV)
 		PIE = PIE(PTS,FGM,FTM,FGA,FTA,DREB,OREB,AST,STL,BLK,PF,TO,GmPTS,GmFGM,GmFTM,GmFGA,GmFTA,GmDREB,GmOREB,GmAST,GmSTL,GmBLK,GmPF,GmTO)
 
+		#adding results to dictionary 
+		data["TmPoss"] = TmPoss
+		data["OppPoss"] = OppPoss
+		data["PProdAst"] = PProdAst
+		data["q5"] = q5
+		data["q12"] = q12
+		data["ASTPart"] = ASTPart
+		data["FTPart"] = FTPart
+		data["TmScorPoss"] = TmScorPoss
+		data["TmOREB_pect"] = TmOREB_pect
+		data["TmPlay"] = TmPlay
+		data["FTmPoss"] = FTmPoss
+		data["DOREB_perc"] = DOREB_perc
+		data["DFG_perc"] = DFG_perc
+		data["eFG_perc"] = eFG_perc
+		data["Turnover_perc"] = Turnover_perc
+		data["FTr"] = FTr
+		data["FG_2_perc"] = FG_2_perc
+		data["FG_3_perc"] = FG_3_perc
+		data["FGr_2"] = FGr_2
+		data["FGr_3"] = FGr_3
+		data["Usage_Rate"] = Usage_Rate
+		data["ASTPart"] = ASTPart
+		data["ASTr"] = ASTr
+		data["AST_Ratio"] = AST_Ratio
+		data["OppPtsPScorPoss"] = OppPtsPScorPoss
+		data["TS_perc"] = TS_perc
+		data["Total_REB_pect"] = Total_REB_pect
+		data["BLK_perc"] = BLK_perc
+		data["Game_Score"] = Game_Score
+		data["PIE"] = PIE
+
+
+
 
 		#second level calculation 
 		TmORTG = TmORTG (TmPoints, TmPoss)
@@ -97,6 +131,20 @@ def stats_calculation(data):
 		Pace = Pace(TmPoss,OppPoss,TmMIN)
 		STL_perc = STL_perc(STL,TmMIN,MIN,OppPoss)
 
+		#adding results to dictionary 
+		data["TmORTG"] = TmORTG
+		data["qAST"] = qAST
+		data["TmPlay_pect"] = TmPlay_pect
+		data["FGmPoss"] = FGmPoss
+		data["Team_Floor_Percentage"] = Team_Floor_Percentage
+		data["TmDRTG"] = TmDRTG
+		data["FMwt"] = FMwt
+		data["Pace"] = Pace
+		data["STL_perc"] = STL_perc
+
+
+		
+
 
 		#third level calculation 
 		PProdFG = PProdFG(FGM,PTS,FTM,FGA,qAST,FGM_3)
@@ -106,16 +154,43 @@ def stats_calculation(data):
 		Stops_2 = Stops_2(OppFGA,OppFGM,TmBLK,TmMIN,FMwt,DOREB_perc,OppTOV,TmSTL,MIN,PF,TmPF,OppFTA,OppFTM)
 
 
+		#adding results to dictionary 
+		data["PProdFG"] = PProdFG
+		data["FGPart"] = FGPart
+		data["TmOREBWgt"] = TmOREBWgt
+		data["Stops_1"] = Stops_1
+		data["Stops_2"] = Stops_2 
+
+
+
+
+
+
+
+
+
 		#forth level calculation 
 		PProdOREB = PProdOREB(ORB,TmOREBWgt,TmPlay_pect,TmPTS,TmFGM,TmFTM,TmFTA)
 		OREBPart = OREBPart(OREB,TmOREBWgt,TmPlay_pect)
 		Stops = Stops(Stops_1,Stops_2)
 
+
+		#adding results to dictionary 
+		data["PProdOREB"] = PProdOREB
+		data["OREBPart"] = OREBPart
+		data["Stops"] = Stops
+
+
 		#fifth level calculation 
-		PProd = PProd(PProdFG, PProdAst,FTM,TmOREB,
-		TMScorPoss,TmOREBWgt,TmPlay_pect,PProdOREB)
+		PProd = PProd(PProdFG, PProdAst,FTM,TmOREB,TmScorPoss,TmOREBWgt,TmPlay_pect,PProdOREB)
 		ScPoss = ScPoss(FGPart,ASTPart,FTPart,TmOREB,TmScorPoss,TmOREBWgt,TmPlay_pect,OREBPart)
 		Stop_perc = Stop_perc(Stops,OppMIN,TmPoss,MIN)
+
+		#adding results to dictionary 
+		data["PProd"] = PProd
+		data["ScPoss"] = ScPoss
+		data["Stop_perc"] = Stop_perc 
+
 
 
 		#sixth level calculation 
@@ -123,9 +198,20 @@ def stats_calculation(data):
 		DRTG = DRTG(TmDRTG,OppPtsPScorPoss,Stop_perc)
 
 
+		#adding results to dictinory 
+		data["TotPoss"] = TotPoss
+		data["DRTG"] = DRTG 
+
+
 		#seventh level calculation 
 		Individual_Offensize_Rating = Individual_Offensize_Rating(PProd, TotPoss)
 		Individual_Floor_Percentage = Individual_Floor_Percentage(ScPoss,TotPoss)
+
+		#adding results to dictinory 
+		data["Individual_Offensize_Rating"] = Individual_Offensize_Rating
+		data["Individual_Floor_Percentage"] = Individual_Floor_Percentage
+
+
 
 
 
