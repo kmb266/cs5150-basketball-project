0f85bfdeaa0b9f84c718782136a159bcd42a73b4
Kemal Hasan Atay <kha26@cornell.edu>
2018-05-06 19:08:40 -0400

    Didnt push before

--- app/data_manager.py
+++ app/data_manager.py
@@ -1,70 +1,117 @@
 import sys, json
 from data_retriever import masterQuery
 
+def sampleForm():
+    form = {
+  "page": "players",
+  "position": [],
+  "team": [
+    "COR"
+  ],
+  "opponent": [
+    "COL"
+  ],
+  "in": [],
+  "out": [],
+  "upOrDown": [
+    "withIn",
+    None
+  ],
+  "season": [
+    "2018"
+  ],
+  "gametime": {
+    "slider": {
+      "start": {
+        "clock": "20:00",
+        "sec": -2400
+      },
+      "end": {
+        "clock": "00:00",
+        "sec": 0
+      }
+    },
+    "sliderExtra": {
+      "start": {
+        "clock": "20:00",
+        "sec": -2400
+      },
+      "end": {
+        "clock": "00:00",
+        "sec": 0
+      }
+    },
+    "multipleTimeFrames": False
+  },
+  "location": {
+    "home": True,
+    "away": True,
+    "neutral": True
+  },
+  "outcome": {
+    "wins": True,
+    "losses": True
+  },
+  "overtime": {
+    "ot1": False,
+    "ot2": False,
+    "ot3": False,
+    "ot4": False,
+    "ot5": False,
+    "ot6": False,
+    "onlyQueryOT": False
+  }
+}
+    return form
+
+def getAverages(players_score, team_score):
+    attributes = ["FG", "FGA3", "3PT", "FTA", "FT", "TP", "OREB",
+                "DREB", "REB", "AST", "STL", "BLK", "TO", "PF", "PTS"]
+    players_boxscore = []
+    teams_boxscore = []
+
+    for values in players_score:
+        games = values["games"]
+        player = {"name" : values["name"], "team" : values["team"]}
+        games_played = len(games)
+
+        for attribute in attributes:
+            for game, game_boxscore in games.items():
+                if attribute in player:
+                    player[attribute] += game_boxscore[attribute]
+                else:
+                    player[attribute] = game_boxscore[attribute]
+            player[attribute] = round(player[attribute] / games_played, 2)
+
+        players_boxscore.append(player)
+
+    for team_id, values in team_score.items():
+        games = values["games"]
+        team = {"team_id" : team_id}
+        games_played = len(games)
+
+        for attribute in attributes:
+            for game, game_boxscore in games.items():
+                if attribute in team:
+                    team[attribute] += game_boxscore[attribute]
+                else:
+                    team[attribute] = game_boxscore[attribute]
+            team[attribute] = round(team[attribute] / games_played, 2)
+
+        teams_boxscore.append(team)
+
+    return (players_boxscore, teams_boxscore)
+
 '''
 retrieve the data from the backend
 '''
 def retrieveData(form):
-    data = masterQuery(form)
+    (players_score, team_score) = masterQuery(form)
+    (players_score, team_score) = getAverages(players_score, team_score)
     #data = json.dumps(data)
-    final = {"dataTab" : "players", "data" : data}
+    final = {"dataTab" : "players", "data" : players_score, "teamOverall" : team_score}
     final = json.dumps(final)
     return final
-    '''
-    data = {"dataTab" : "players",
-            "data" : [
-                {
-                    "name" : "Kyle Brown",
-                    "jersey" : "1",
-                    "FG" : 8,
-                    "3PT" : 3,
-                    "FT" : 2,
-                    "OREB" : 1,
-                    "DREB" : 7,
-                    "REB" : 8,
-                    "AST" : 5,
-                    "STL" : 2,
-                    "BLK" : 1,
-                    "TO" : 2,
-                    "PF" : 3,
-                    "PTS" : 21
-                },
-                {
-                    "name" : "Matt Morgan",
-                    "jersey" : "10",
-                    "FG" : 5,
-                    "3PT" : 4,
-                    "FT" : 3,
-                    "OREB" : 2,
-                    "DREB" : 3,
-                    "REB" : 5,
-                    "AST" : 7,
-                    "STL" : 1,
-                    "BLK" : 0,
-                    "TO" : 2,
-                    "PF" : 3,
-                    "PTS" : 17
-                },
-                {
-                    "name" : "Jack Gordon",
-                    "jersey" : "32",
-                    "FG" : 7,
-                    "3PT" : 3,
-                    "FT" : 1,
-                    "OREB" : 4,
-                    "DREB" : 1,
-                    "REB" : 5,
-                    "AST" : 10,
-                    "STL" : 4,
-                    "BLK" : 1,
-                    "TO" : 4,
-                    "PF" : 4,
-                    "PTS" : 18
-                }
-            ]
-        }
-    return data
-    '''
 
 def tidyForm(form):
     if ('season' in form) == False:
@@ -81,7 +128,8 @@ def getForm():
 
 def main():
     form = getForm()
-    #form = {"team" : ["COR"], "opponent" : []}
+    #form = sampleForm()
+
     form = tidyForm(form)
     data = retrieveData(form)
 
