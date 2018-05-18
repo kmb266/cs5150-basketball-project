d8ddc4b09e2116480bcade50bf626c70d8b30dd2
sarvar <sdhillon4939@gmail.com>
2018-05-18 01:09:16 -0400

    it works

--- app/data_manager.py
+++ app/data_manager.py
@@ -304,8 +304,8 @@ def getForm():
     return json.loads(lines[0])
 
 def main():
-    # form = getForm()
-    form = sampleForm()
+    form = getForm()
+    # form = sampleForm()
 
     form = tidyForm(form)
     try:
--- app/src/global.vars.ts
+++ app/src/global.vars.ts
@@ -12,7 +12,7 @@ export const navHeight: number = 50;
 export const pages: Array<string> = ["players", "teams","games"];
 export const numPages: number = pages.length;
 
-const PROD = false;
+const PROD = true;
 
 export const getSeason = () => {
   /*
