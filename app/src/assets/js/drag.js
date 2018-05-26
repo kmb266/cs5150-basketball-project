d166ebf3d947488f2eb5f882b06aea08c2a62e85
sarvar <sdhillon4939@gmail.com>
2018-05-26 16:02:15 -0400

    adv stat draggable as well

--- app/src/assets/js/drag.js
+++ app/src/assets/js/drag.js
@@ -3,11 +3,8 @@ function dragify() {
     console.log("DEBUG: in dragify()");
     var tableDragger = require('table-dragger');
     var el = document.getElementById('players-table');
-    var dragger = tableDragger(el, {
-      mode: 'column',
-      dragHandler: '.handle',
-      onlyBody: true,
-      animation: 300
-    });
+    var dragger = tableDragger(el);
 
+    var stats = document.getElementById('playersadv-table');
+    var stat_dragger = tableDragger(stats);
 }
